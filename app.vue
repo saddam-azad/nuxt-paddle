ceb5f3e883d1e1327e54539088503107f47223ec
Saddam Azad <saddam.azad@me.com>
2024-05-23 16:08:26 +0600

    hygiene

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ const paddle = ref<Paddle | undefined>(undefined);
 onMounted(async () => {
   const paddleInstance = await initializePaddle({
     environment: 'sandbox',
-    token: 'live_086c73ae87243fb1d1413cc85a4' 
+    token: '' 
   });
   if (paddleInstance) {
     paddle.value = paddleInstance;
@@ -27,7 +27,7 @@ const openCheckout = () => {
     items: [
       {
         quantity: 1,
-        priceId: 'pri_01hq52466a4tqfc4bsz0f62f87'
+        priceId: ''
       }
     ]
   })
